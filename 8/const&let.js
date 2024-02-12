41e77a71ac4c43fbdeb9cef945f800c97ac4f062
Aditya Priyanshu <adityap3055@gmail.com>
2024-02-12 17:04:49 +0530

    const&let

---
+++ 8/const&let.js
@@ -0,0 +1,15 @@
+//  Const is declared for those whose value will not be changed in future.
+//  It cannot be re-declared or updated.
+const a = 10;
+console.log(a);
+
+// Let - We use let when we need to change the value of the variable.
+// It can be updated but cannot be re-declared.
+let b = 30;
+console.log(b);
+b = "Hi the value and type changed for b"
+console.log(b);
+
+
+
+// from es6 we use const and let and not var.
