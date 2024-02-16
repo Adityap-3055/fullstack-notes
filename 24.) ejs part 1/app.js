2abc49bd76c299ed977ead161b0ac3c9d959a8a8
Aditya Priyanshu <adityap3055@gmail.com>
2024-02-16 17:18:00 +0530

    ejs part 1

--- 24.) ejs part 1/app.js
+++ 24.) ejs part 1/app.js
@@ -1,6 +1,20 @@
 // install ejs using npm install ejs
-// To start with the ejs we need to first set our app to use it.
-// we simply add this using: app.set('view engine', 'ejs')
+/*
+To start with the ejs first we need to set our app to use it. We can simply add this using app.set('view engine', 'ejs')
+
+This app.set('view engine', 'ejs') will look into views folder on the same path. So we need to create views folder for writing those frontend templates.
+We are starting the profile page. So we’ll create a profile.ejs file inside the views folder.
+Just create a basic html inside of profile.ejs file.
+
+We’ll use res.render method to render the file.
+
+It’s already know where to look. So we’ve render the files, but we didn’t pass our data to the html. How to do that ?
+
+We can send a object in render method while rendering the template and use that object to get the value of username.
+
+Now we can easily access the value using <%= person %> in the profile.ejs file
+See the result in frontend
+*/
 const express = require('express')
 const app = express()
 
