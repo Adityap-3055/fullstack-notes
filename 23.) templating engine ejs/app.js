9de4b45ebbf4cfb91274699c172b74fd5797067b
Aditya Priyanshu <adityap3055@gmail.com>
2024-02-16 16:47:39 +0530

    ejs into

---
+++ 23.) templating engine ejs/app.js
@@ -0,0 +1,31 @@
+/**
+
+To creating quick node application we can use some frontend template engine.
+
+There are many different templating engines
+
+EJS
+Mustache
+Handlebars
+Underscore
+Pug
+Marko
+We can use any of them to do the frontend while making the api.
+
+I’m going to use EJS to show you how to do this.
+
+Start with the official page – https://ejs.co/
+
+
+You can check the features of it
+
+
+Installing it is super easy
+
+
+So finally our package.json
+"dependencies": {
+    "ejs": "^3.1.9",
+    "express": "^4.18.2"
+  }
+ */
