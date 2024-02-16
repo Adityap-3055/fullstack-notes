0245add12095446b5ed803094940247cbd761510
Aditya Priyanshu <adityap3055@gmail.com>
2024-02-16 15:04:50 +0530

    express

---
+++ 18.) express/app.js
@@ -0,0 +1,13 @@
+/*
+
+Express.js is a node.js framework
+Frameworks helps us to write code easily and maintain a standard of our codes.
+This really needed when multiple team member are working on the same code.
+
+Express is
+- Fast & Simple
+- Speedy
+- Scalable
+- Easy to Learn
+
+*/
