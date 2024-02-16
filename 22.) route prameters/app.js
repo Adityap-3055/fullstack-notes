3a64f7947c18d3adca17b55af7565c6463ad9361
Aditya Priyanshu <adityap3055@gmail.com>
2024-02-16 15:37:21 +0530

    /params

--- 22.) route prameters/app.js
+++ 22.) route prameters/app.js
@@ -1,3 +1,16 @@
+/**
+ * Up until now we never passed any id with our route. What if we need to go to a specific profile ?
+
+Like – profile/10 How we can load different using those id ?
+
+We can get what user request after profile, how we can get it ? In this case they request for the 10 right ?
+
+To catch the 10 we need to make a specific route like ‘/profile/:id’ this id will catch what request send after profile. So how to catch it ?
+
+Just use req.params.id this will give us the id
+ */
+
+
 const express = require('express')
 const app = express()
 
