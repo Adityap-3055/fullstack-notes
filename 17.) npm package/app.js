d98a22d657f5cb2c6bea7171814736ee991a928d
Aditya Priyanshu <adityap3055@gmail.com>
2024-02-16 15:02:36 +0530

    npm package

---
+++ 17.) npm package/app.js
@@ -0,0 +1,16 @@
+//npm - Node Package Manager
+
+/*
+ *  It is the default package manager for javascript runtime enviroment Node.js
+    A package manager is a collection of software tools that automates the process
+    of upgrading, configuring and removing computer programs for a computer's
+    operating system in a consistent manner.
+
+ *  So basically npm is a system we can use to do different things with 
+    our codebase to manage properly. 
+    
+ *  Now it's time to use npm. We can start configure a project using `npm init`.
+    It will promt up some question and basically we are going to say yes to them.
+    So shortcut way is just add -yes after everything. like - `npm init -yes`
+
+*/
